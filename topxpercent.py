04d64b0e43e9dfca4867174e84c13d69eadff050
Pranav Salunke <pranav.salunke@gmail.com>
2019-05-31 18:43:29 -0700

    fixed country list, topxperecent now works too.....
    topxpercent shows the top countries that make up x% of the worlds
    population together
    
    country list had to match exactly as the country names showed up in the UN
    data

--- topxpercent.py
+++ topxpercent.py
@@ -1,4 +1,5 @@
 import pandas
+import re
 
 # the top locations that make up targetPercent of the worlds population for the given year
 # data taken from the given file (assumed to be the output of the reformated population data)
@@ -6,7 +7,7 @@ import pandas
 
 fileName = "data/TotalPopulation_Medium.csv"
 year = "2020"
-targetPercent = 0.4  # 0 to 1
+targetPercent = 0.6  # 0 to 1
 
 popDF = pandas.read_csv(fileName)
 
@@ -19,7 +20,26 @@ worldTotal = yearTotals.loc[yearTotals["Location"] == "World"][year].item()  # g
 print(worldTotal)
 
 # remove all non-country locations
-### DO BE DONE ###
+# read in list of countries
+with open("country-list.txt") as country_list:
+    countries_file = country_list.read()
+
+# Remove bad characters and unwanted spaces
+# countries = re.sub('[^a-zA-Z\s]', '', countries_file).split("\n")  # split on new line instead of whitespace
+countries = countries_file.split("\n")  # split on new line instead of whitespace
+countries = [c for c in countries if c]  # remove empty line
+# print(countries)
+countriesDF = pandas.DataFrame(data={"countries": countries})
+# print(countriesDF.countries)
+
+# make a common dataframe that has only countries and removes non-countries, dropping na values
+common = countriesDF.merge(yearTotals, how='inner', left_on=['countries'], right_on=['Location'])
+yearTotals = common.dropna()
+# print(yearTotals)
+# print(common)
+# common = common.drop(['Location'], axis=1)
+# print(common)
+
 
 # find the percent of the world's total by location and add a column with that
 yearTotals["Percent"] = yearTotals[year]/worldTotal
