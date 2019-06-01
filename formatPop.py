ffd850b8971b4baf50599cb39079d0c989a9fdbc
Pranav Salunke <pranav.salunke@gmail.com>
2019-06-01 12:28:48 -0700

    fix formatPop (again). edge cases taken care of...
    last location was not showing
    fixed data sets in /data

--- formatPop.py
+++ formatPop.py
@@ -6,6 +6,34 @@ import time
 ORIG_DATA = "data/WPP2017_TotalPopulationBySex.csv"
 
 
+# helper function to write the three rows per location
+def writeLocRows(filename, trackingVars, data):
+    currLoc, currVar = trackingVars
+    maleData, femaleData, totalData = data
+
+    rows = []
+    # remember new header: ["Location", "Variant", "Group", 1959, ..., 2100]
+    # male data
+    maleRow = [currLoc, currVar, "Male"] + maleData
+    rows.append(maleRow)
+    # female data
+    femaleRow = [currLoc, currVar, "Female"] + femaleData
+    rows.append(femaleRow)
+    # total data
+    totalRow = [currLoc, currVar, "Total"] + totalData
+    rows.append(totalRow)
+
+    # print(maleRow)
+    # print(femaleRow)
+    # print(totalRow)
+
+    # write all the rows
+    with open(filename, "a", newline="") as newFile:
+        # append to the new file
+        newWriter = csv.writer(newFile)
+        newWriter.writerows(rows)
+
+
 # break the original file into other, reformated files
 # we ignore midPeriod, and ID values in the reformatted files
 with open(ORIG_DATA, "r") as origdata:
@@ -27,8 +55,8 @@ with open(ORIG_DATA, "r") as origdata:
     currLoc = firstLine[1]
     currVar = firstLine[3]
     var = firstLine[3].title().replace(" ", "")  # make "Hello world"  to "HelloWorld"
-    print("New variant: %s" % (var))
     currFileName = baseFile.replace("[[VARIANT]]", var)
+    print("Now writing to: %s" % (currFileName))
     # print(currFileName)
 
     # write new header
@@ -51,11 +79,29 @@ with open(ORIG_DATA, "r") as origdata:
         lineLoc = line[1]
         lineVar = line[3]
 
+        if lineLoc != currLoc:  # new location
+
+            # write location data
+            trackingVars = (currLoc, currVar)
+            data = (maleData, femaleData, totalData)
+            writeLocRows(currFileName, trackingVars, data)
+
+            # reset tracker for location
+            currLoc = lineLoc
+
+            # and reset data lists
+            maleData.clear()
+            femaleData.clear()
+            totalData.clear()
+
         if lineVar != currVar:  # new variant, want to make new file
             # the way the orig data is done, if the variant changes, so does the location
+            #   which means the location block above is also run
+
             # create new file name
             var = line[3].title().replace(" ", "")  # make "Hello world"  to "HelloWorld"
             currFileName = baseFile.replace("[[VARIANT]]", var)
+            print("Now writing to: %s" % (currFileName))
 
             # write new header to new file
             with open(currFileName, "w", newline="") as newFile:
@@ -63,49 +109,18 @@ with open(ORIG_DATA, "r") as origdata:
                 newWriter.writerow(newHeader)
 
             # reset tracking variables
-            currLoc = line[1]
-            currVar = line[3]
+            # currLoc already set in location block above
+            currVar = lineVar
 
-            # clear the data lists (this line will be added below)
-            maleData.clear()
-            femaleData.clear()
-            totalData.clear()
-
-            print("New variant: %s" % (var))
-
-        if lineLoc != currLoc:  # new location
-            rows = []
-            # remember new header: ["Location", "Variant", "Group", 1959, ..., 2100]
-            # male data
-            maleRow = [currLoc, currVar, "Male"] + maleData
-            rows.append(maleRow)
-            # female data
-            femaleRow = [currLoc, currVar, "Female"] + femaleData
-            rows.append(femaleRow)
-            # total data
-            totalRow = [currLoc, currVar, "Total"] + totalData
-            rows.append(totalRow)
-
-            # print(maleRow)
-            # print(femaleRow)
-            # print(totalRow)
-
-            # write all the rows
-            with open(currFileName, "a", newline="") as newFile:
-                # append to the new file
-                newWriter = csv.writer(newFile)
-                newWriter.writerows(rows)
-
-            # reset tracker for location
-            currLoc = line[1]
-
-            # and reset data lists
-            maleData.clear()
-            femaleData.clear()
-            totalData.clear()
+            # data lists already cleared in location block above
 
         # add the populations to the data list
         # will be empty if needed (ie. new variant or new location)
         maleData.append(line[6])
         femaleData.append(line[7])
         totalData.append(line[8])
+
+    # write the stuff that hasnt been written yet ()
+    trackingVars = (currLoc, currVar)
+    data = (maleData, femaleData, totalData)
+    writeLocRows(currFileName, trackingVars, data)
